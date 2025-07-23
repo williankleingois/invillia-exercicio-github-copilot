3abbb1a542f66202db159490af9f70fcf5272c98
Willian Klein de Gois <145627399+williankleingois@users.noreply.github.com>
2025-07-23 19:00:14 +0000

    Corrige formatação do comentário de validação de inscrição

--- src/app.py
+++ src/app.py
@@ -101,7 +101,7 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    #Validar se o aluno já está inscrito
+    # Validar se o aluno já está inscrito
     if email in activity["participants"]:       
         raise HTTPException(status_code=400, detail="Already signed up for this activity")     
 
